e60d7f3d9929dc0aa143aed6e77d13af34690419
MaximeVives <44499746+MaximeVives@users.noreply.github.com>
2022-02-22 23:44:13 +0100

    Content structure down -> Need content

--- asset/js/script.js
+++ asset/js/script.js
@@ -26,3 +26,38 @@ document.querySelector('.bar').addEventListener('click', function (e) {
     document.querySelector('.nav-panel').classList.toggle('active');
     document.querySelector('.bar').classList.toggle('off');
 });
+
+
+document.querySelectorAll('.library-item').forEach(function (element) {
+    element.addEventListener('click', function (e) {
+
+        let img = ""
+
+        if(e.target.classList.contains("be-full")){
+            img = e.target.parentNode.childNodes[1].cloneNode(true).getAttribute("src")
+        }
+
+        else if(e.target.classList.contains("rc-text")){
+            img = e.target.parentNode.parentNode.childNodes[1].cloneNode(true).getAttribute("src")
+        }
+        else if(e.target.classList.contains("main-text") || e.target.classList.contains("second-text")){
+            img = e.target.parentNode.parentNode.parentNode.childNodes[1].cloneNode(true).getAttribute("src")
+        }
+
+
+        document.querySelector(".focus").style.display= "flex"
+        document.querySelector("body").style.overflow= "hidden"
+        document.querySelector(".focus").childNodes[1].setAttribute("src", img)
+
+        document.querySelector(".focus").addEventListener('click', e => {
+            if(e.target !== e.currentTarget) console.log(e.target);
+            else{
+                document.querySelector(".focus").style.display= "none"
+                document.querySelector("body").style.overflow= "initial"
+            }
+        });
+
+
+
+    });
+});
